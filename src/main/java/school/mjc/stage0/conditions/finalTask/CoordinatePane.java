79f54d6b4f26839c3f12564705792ab8cd4b362d
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-08-30 01:09:01 +0400

    update

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -3,13 +3,13 @@ package school.mjc.stage0.conditions.finalTask;
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
         if (x > 0 && y > 0) {
-            System.out.println("First");
+            System.out.println("first");
         } else if (x < 0 && y > 0) {
-            System.out.println("Second");
+            System.out.println("second");
         } else if (x < 0 && y < 0) {
-            System.out.println("Third");
+            System.out.println("third");
         } else if (x > 0 && y < 0) {
-            System.out.println("Fourth");
+            System.out.println("fourth");
         } else if (x == 0 && y == 0) {
             System.out.println("zero");
         }
