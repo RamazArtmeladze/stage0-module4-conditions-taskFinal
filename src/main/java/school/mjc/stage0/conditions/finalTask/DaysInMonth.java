859f9fd98073c692f49caea74d3686f09fc860b2
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-08-30 00:59:53 +0400

    update

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,34 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if (year < 0 || month < 1 || month > 12) {
+            System.out.println("Invalid date");
+            return;
+        }
 
+        int daysInMonth;
+
+        switch (month) {
+            case 2: // February
+                if (isLeapYear(year)) {
+                    daysInMonth = 29;
+                } else {
+                    daysInMonth = 28;
+                }
+                break;
+            case 4: case 6: case 9: case 11: // April, June, September, November
+                daysInMonth = 30;
+                break;
+            default: // January, March, May, July, August, October, December
+                daysInMonth = 31;
+                break;
+        }
+
+        System.out.println("Number of days in month " + month + " of year " + year + ": " + daysInMonth);
+    }
+
+    private boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
     }
 }
+
--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -2,5 +2,23 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class SeasonDeterminer {
     public void tellTheSeason(int monthNumber) {
+        if (monthNumber >= 1 && monthNumber <= 12) {
+            String season;
+
+            if (monthNumber >= 3 && monthNumber <= 5) {
+                season = "Spring";
+            } else if (monthNumber >= 6 && monthNumber <= 8) {
+                season = "Summer";
+            } else if (monthNumber >= 9 && monthNumber <= 11) {
+                season = "Fall";
+            } else {
+                season = "Winter";
+            }
+
+            System.out.println(season);
+        } else {
+            System.out.println("Wrong month number");
+        }
     }
 }
+
